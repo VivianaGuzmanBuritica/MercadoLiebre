7103dceca7d30e907c0531a6eaaf24bf3caa5cb9
unknown <vivianaguzmanburitica@gmail.com>
2021-05-27 18:23:06 -0500

    deploy

---
+++ proyecto/app.js
@@ -0,0 +1,14 @@
+const path= require('path');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,'./public')))
+
+app.set("port", process.env.PROT || 3000)
+
+app.listen(app.get("port"), ()=> console.log('Server start  in http://local host:'+app.js))
+
+app.get('/', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'home.html')))
+app.get('/register', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'register.html')))
+app.get('/login', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'login.html')))
