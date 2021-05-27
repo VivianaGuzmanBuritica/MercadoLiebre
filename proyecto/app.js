7d613d0f19b505c809b73872bc2fb6c7da75a8f8
unknown <vivianaguzmanburitica@gmail.com>
2021-05-27 18:42:24 -0500

    deploy

--- proyecto/app.js
+++ proyecto/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname,'./public')))
 
-app.set("port", process.env.PROT || 3000)
+app.set("port", process.env.PORT || 3000)
 
 app.listen(app.get("port"), ()=> console.log('Server start  in http://local host:'+app.js))
 
